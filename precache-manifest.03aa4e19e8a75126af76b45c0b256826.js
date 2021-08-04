63d0f1e365da1cacada1a7ddef5188ea801dfc76
hogyun3709 <hogyun3709@gmail.com>
2021-08-04 14:03:06 +0900

    pwa 적용

---
+++ precache-manifest.03aa4e19e8a75126af76b45c0b256826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76d9f6422a44878b2fa",
+    "url": "/css/app.bf7c5bc0.css"
+  },
+  {
+    "revision": "e4dd7c6c0bf8825c9b1cf95bcced4bcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e76d9f6422a44878b2fa",
+    "url": "/js/app.867e8bf8.js"
+  },
+  {
+    "revision": "0bdd5dc79a4197918cf9",
+    "url": "/js/chunk-vendors.dc3dc2af.js"
+  },
+  {
+    "revision": "e1d70d9a017080280c305c0159eabe20",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
