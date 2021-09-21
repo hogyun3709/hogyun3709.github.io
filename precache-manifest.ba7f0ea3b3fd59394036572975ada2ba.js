27f714c0234cade482ed26b3d9d328a1cfb068b0
hogyun3709 <hogyun3709@gmail.com>
2021-09-21 20:20:25 +0900

    카메라앱 테스트

---
+++ precache-manifest.ba7f0ea3b3fd59394036572975ada2ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b394c1cd4d10b30d88",
+    "url": "/css/app.5d871a8a.css"
+  },
+  {
+    "revision": "1c17ed7c99cdc684494d040a96c6bfee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b394c1cd4d10b30d88",
+    "url": "/js/app.f5ac3079.js"
+  },
+  {
+    "revision": "23d1e8e113da1ed4cf75",
+    "url": "/js/chunk-vendors.17958fe6.js"
+  },
+  {
+    "revision": "e1d70d9a017080280c305c0159eabe20",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
